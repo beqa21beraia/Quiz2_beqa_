f21666beaf52285a82148fe29b31cebadecd3f35
giorgi4235 <giorgi.chincharauli.1@btu.edu.ge>
2024-12-26 13:24:14 +0400

    Added ViewPager to Home page

--- app/src/main/java/com/example/mobileapplications22/home/HomeFragment.kt
+++ app/src/main/java/com/example/mobileapplications22/home/HomeFragment.kt
@@ -5,19 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mobileapplications22.R
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.example.mobileapplications22.databinding.FragmentHomeBinding
+import com.example.mobileapplications22.adapters.ViewPagerAdapter
 
 class HomeFragment : Fragment() {
-//    uihfoiasd
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var adapter: ViewPagerAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        val fragments = listOf(
+            BlankFragment1(),
+            PagerFragment.newInstance(),
+            BlankFragment2(),
+        )
+
+        val adapter = ViewPagerAdapter(requireActivity(), fragments)
+        binding.HomeViewPager.adapter = adapter
+
+        binding.HomeViewPager.setCurrentItem(1, true)
+    }
     companion object {
         @JvmStatic
         fun newInstance() = HomeFragment()
